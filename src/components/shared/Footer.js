59dc9c84cbf8183f55b0b8c335cbda67d8fd7af9
Ana Sala <georgina.sala18@gmail.com>
2022-03-23 14:00:19 -0600

    Footer component css styling

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,9 +1,9 @@
 function Footer() {
     return(
-        <footer>
-            <small>
-                Open-source code by 
-                    <a href="https://github.com/AGSS18/goals-app" target="_blank">Ana Sala</a>
+        <footer className="Footer box-shadow">
+            <small className="footer-coder">
+                Open-source code by  
+                    <a href="https://github.com/AGSS18/goals-app" target="_blank"> Ana Sala</a>
             </small>
         </footer>
     );
